7e7366fd5d9e3a4448e871365c0053b1897ae18e
El'dar Garipov <e_garipow@mail.ru>
2023-04-02 01:18:25 +0300

    Написал функцию сортировки слиянием

---
+++ Parallel_sort/parallel_sort.py
@@ -0,0 +1,33 @@
+'''Программа параллельной сортировки'''
+
+
+# функция сортировки слиянием
+def merge_sort(lst):
+    
+    def merge_lists(left, right):
+        i, j = 0, 0
+        sort_list = []
+        while len(left) > i and len(right) > j:
+            if left[i] < right[j]:
+                sort_list.append(left[i])
+                i += 1
+            else:
+                sort_list.append(right[j])
+                j += 1
+        if len(left) > i:
+            sort_list += left[i:]
+        if len(right) > j:
+            sort_list += right[j:]
+        return sort_list
+    
+    if len(lst) == 1:
+        return lst
+    middle = len(lst) // 2
+    left = merge_sort(lst[:middle])
+    right = merge_sort(lst[middle:])
+    return merge_lists(left, right)
+
+
+# принимаемые параметры
+file_name = input()  # имя файла для сортировки
+n = int(input())  # количество чисел из файла для загрузки в память
