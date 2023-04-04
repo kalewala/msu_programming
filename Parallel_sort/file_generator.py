72f0871df16648312ed275516e7ad470ef6c3162
El'dar Garipov <e_garipow@mail.ru>
2023-04-04 03:27:16 +0300

    Написал генератор случайного списка для сортировки

---
+++ Parallel_sort/file_generator.py
@@ -0,0 +1,19 @@
+"""
+Генератор бинарного файла 32-битных целых чисел
+
+Python 3.10.7
+    numpy 1.23.4
+"""
+
+import numpy as np
+
+
+# случайный массив int32
+lst = np.random.randint(-2147483648,
+                        high=2147483647,
+                        size=20,
+                        dtype="int32")
+
+# запись бинарного файла
+with open('Parallel_sort/input.npy', 'wb') as f:
+    np.save(f, lst)
